dc4d6f8a75b1d87eeafd107d84df69817e957487
sunnywizy <sundayobasi222@gmail.com>
2025-09-14 13:51:40 +0100

    I just added some JavaScript files to my repository

---
+++ 1-multi_languages.js
@@ -0,0 +1,10 @@
+// This script uses the console.log() command to print three separate lines of text.
+
+// Print the first line of text.
+console.log("C is fun");
+
+// Print the second line of text.
+console.log("Python is cool");
+
+// Print the third line of text.
+console.log("JavaScript is amazing");
